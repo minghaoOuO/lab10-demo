bac1e622c6a17fed3e1d32e1abdb65411c87626e
minghaoOuO <ming.hao.hsieh0914@gmail.com>
2024-05-14 10:41:48 +0800

    initial commit

---
+++ src/settings.ed70c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"b04d9"}};
